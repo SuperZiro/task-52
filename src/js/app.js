6d9799126dd526a18b47fb6ad9cdfc74d7706962
SuperZiro <vankogalata04@gmail.com>
2023-01-14 19:27:20 +0200

    did js

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,12 @@
 import "../scss/app.scss";
 
+var article = "<article class=\"message\">hi</article><article class=\"message\">my name is Ivan</article><article class=\"message\">good to know you tho</article><article class=\"message\">whassup</article><article class=\"message\">bye.</article>";
+
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
+  window.addEventListener("click",() =>{
+    document.body.innerHTML = article;
+  })
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
